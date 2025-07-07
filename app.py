44648b090442dc8533ffeae609a48e100d93f22e
Roshan Yadav <yaroshan5@gmail.com>
2025-07-07 11:54:05 +0545

    Added HTML and CSS code

--- app.py
+++ app.py
@@ -2,7 +2,21 @@ from flask import Flask, render_template
 app= Flask(__name__)
 
 
+
+Skills=[
+   {'name': 'Python', 'level': 'Advanced'},
+   {'name': 'JavaScript', 'level': 'Intermediate'},
+   {'name': 'HTML/CSS', 'level': 'Advanced'},
+   {'name': 'Flask', 'level': 'Intermediate'},
+]
+
+
 @app.route('/')
 
 def homePage():
     return render_template('index.html')
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
